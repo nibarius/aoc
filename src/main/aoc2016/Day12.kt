0ea44f54c95994284c9ad2adea369fdc659627f2
Niklas Barsk <barsk@opera.com>
2018-10-27 21:48:17 +0200

    Add 2016 day 12.

---
+++ src/main/aoc2016/Day12.kt
@@ -0,0 +1,51 @@
+package aoc2016
+
+class Day12(val instructions: List<String>) {
+
+    private val registers = mutableMapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0)
+
+    private fun doInstruction(instructionPointer: Int, instruction: String, params: List<String>): Int {
+        when (instruction) {
+            "cpy" -> registers[params[1]] = valueOf(params[0])
+
+            "inc" -> registers.increase(params[0])
+            "dec" -> registers.decrease(params[0])
+            // Part 2: Make it possible to optimize the assembunny code by adding support for a add instruction
+            "add" -> registers[params[1]] = valueOf(params[0]) + valueOf(params[1])
+            "jnz" -> {
+                if (valueOf(params[0]) != 0) {
+                    return instructionPointer + valueOf(params[1])
+                }
+            }
+
+        }
+        return instructionPointer + 1
+    }
+
+    private fun valueOf(value: String): Int {
+        return try {
+            value.toInt()
+        } catch (_: NumberFormatException) {
+            registers[value]!!
+        }
+    }
+
+    private fun execute() {
+        var instructionPointer = 0
+        while (instructionPointer < instructions.size) {
+            val inst = instructions[instructionPointer].split(" ")
+            instructionPointer = doInstruction(instructionPointer, inst[0], inst.drop(1))
+        }
+    }
+
+    fun solvePart1(): Int {
+        execute()
+        return registers["a"]!!
+    }
+
+    fun solvePart2(): Int {
+        registers["c"] = 1
+        execute()
+        return registers["a"]!!
+    }
+}
