f455b5faf7bc03c50b8996278f8b766eaa201769
Niklas Barsk <barsk@opera.com>
2018-11-15 20:16:30 +0100

    Solve day 23 Part 1.

--- src/main/aoc2016/Day23.kt
+++ src/main/aoc2016/Day23.kt
@@ -1,16 +1,17 @@
 package aoc2016
 
-class Day12(private val instructions: List<String>) {
+class Day23(private val instructions: List<String>) {
 
     fun solvePart1(): Int {
         val bunny = Assembunny(instructions.toMutableList())
+        bunny.registers["a"] = 7
         bunny.execute()
         return bunny.registers["a"]!!
     }
 
     fun solvePart2(): Int {
         val bunny = Assembunny(instructions.toMutableList())
-        bunny.registers["c"] = 1
+        bunny.registers["a"] = 12
         bunny.execute()
         return bunny.registers["a"]!!
     }
