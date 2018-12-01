9a154e1c6c2fe55acb4e14967c716b3c1a45bbc8
Niklas Barsk <nibarius@gmail.com>
2018-12-01 10:21:41 +0100

    2018 Day 1: Chronal Calibration
    1st of December is finally here and the story looks interesting,
    time travel is always fun!
    
    Quick and easy first day.

--- src/main/aoc2018/Day1.kt
+++ src/main/aoc2018/Day1.kt
@@ -1,13 +1,35 @@
 package aoc2018
 
 
-class Day1(input: String) {
+class Day1(input: List<String>) {
+
+    private val parsedInputs = parseInput(input)
+
+    private fun parseInput(input: List<String>): List<Int> {
+        val ret = mutableListOf<Int>()
+        input.forEach { ret.add(it.toInt()) }
+        return ret
+    }
+
+    private fun findFirstRepeatedFrequency(): Int {
+        val seenFrequencies = mutableSetOf(0)
+        var currentFrequency = 0
+        while (true) {
+            parsedInputs.forEach {
+                currentFrequency += it
+                if (seenFrequencies.contains(currentFrequency)) {
+                    return currentFrequency
+                }
+                seenFrequencies.add(currentFrequency)
+            }
+        }
+    }
 
     fun solvePart1(): Int {
-        return -2
+        return parsedInputs.sum()
     }
 
     fun solvePart2(): Int {
-        return -2
+        return findFirstRepeatedFrequency()
     }
 }
