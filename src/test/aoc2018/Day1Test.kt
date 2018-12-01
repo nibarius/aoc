e31275dead9110740e9c34ee7fc89f28b359563e
Niklas Barsk <nibarius@gmail.com>
2018-12-01 10:21:41 +0100

    2018 Day 1: Chronal Calibration
    1st of December is finally here and the story looks interesting,
    time travel is always fun!
    
    Quick and easy first day.

--- src/test/aoc2018/Day1Test.kt
+++ src/test/aoc2018/Day1Test.kt
@@ -3,30 +3,33 @@ package test.aoc2018
 import aoc2018.Day1
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import resourceAsString
+import resourceAsList
 
 class Day1Test {
     @Test
-    fun partOneExampleOne() {
-        assertEquals(-1, Day1("?").solvePart1())
+    fun testPart1Examples() {
+        assertEquals(3, Day1(listOf("+1", "+1", "+1")).solvePart1())
+        assertEquals(0, Day1(listOf("+1", "+1", "-2")).solvePart1())
+        assertEquals(-6, Day1(listOf("-1", "-2", "-3")).solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
-        assertEquals(-1, Day1(resourceAsString("2018/day1.txt")).solvePart1())
+        assertEquals(472, Day1(resourceAsList("2018/day1.txt")).solvePart1())
     }
 
     @Test
-    fun partTwoExample() {
-        assertEquals(-1, Day1("?").solvePart2())
+    fun testPart2Examples() {
+        assertEquals(0, Day1(listOf("+1", "-1")).solvePart1())
+        assertEquals(10, Day1(listOf("+3", "+3", "+4", "-2", "-4")).solvePart2())
+        assertEquals(5, Day1(listOf("-6", "+3", "+8", "+5", "-6")).solvePart2())
+        assertEquals(14, Day1(listOf("+7", "+7", "-2", "-7", "-4")).solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
-        assertEquals(-1, Day1(resourceAsString("2018/day1.txt")).solvePart2())
+        assertEquals(-1, Day1(resourceAsList("2018/day1.txt")).solvePart2())
     }
-
-
 }
 
 
