d5d80731ffbba9271153f7b0b5889de03af90d53
Niklas Barsk <nibarius@gmail.com>
2018-12-02 22:39:48 +0100

    2018 Day 2: Inventory Management System
    Easy problem that gave me a chance to practice using the standard
    library functions groupingBy, zip and with. with and zip makes
    sense, but more practice is probably needed before I can use
    groupingBy intuitively.

---
+++ src/main/aoc2018/Day2.kt
@@ -0,0 +1,44 @@
+package aoc2018
+
+class Day2(private val input: List<String>) {
+
+    private fun calculateChecksum(words: List<String>): Int {
+        var twos = 0
+        var threes = 0
+        for (word in words) {
+            val charCount = word.groupingBy { it }.eachCount()
+            with(charCount.values) {
+                if (contains(2)) twos++
+                if (contains(3)) threes++
+            }
+        }
+        return twos * threes
+    }
+
+    private fun idsAreClose(id1: String, id2: String): Boolean {
+        return id1.zip(id2).count { it.first != it.second } == 1
+    }
+
+    private fun sameCharacters(id1: String, id2: String): String {
+        return id1.filterIndexed { index, c -> c == id2[index] }
+    }
+
+    private fun findBoxes(ids: List<String>): String {
+        for (i in 0 until ids.size) {
+            for (j in i + 1 until ids.size) {
+                if (idsAreClose(ids[i], ids[j])) {
+                    return sameCharacters(ids[i], ids[j])
+                }
+            }
+        }
+        return ""
+    }
+
+    fun solvePart1(): Int {
+        return calculateChecksum(input)
+    }
+
+    fun solvePart2(): String {
+        return findBoxes(input)
+    }
+}
