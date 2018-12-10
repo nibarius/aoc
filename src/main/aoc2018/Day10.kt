caccbb13867b7035b3050694607dd6d838dcc7a0
Niklas Barsk <nibarius@gmail.com>
2018-12-10 21:28:47 +0100

    2018 Day 10: The Stars Align
    Fairly easy, the main problem was to find a good heuristic for when
    the message is visible.

---
+++ src/main/aoc2018/Day10.kt
@@ -0,0 +1,60 @@
+package aoc2018
+
+class Day10(input: List<String>) {
+    private data class Pos(val x: Int, val y: Int) {
+        fun hasNeighbour(allPoints: Set<Pos>): Boolean {
+            return List(3) { nx -> List(3) { ny -> Pos(x - 1 + nx, y - 1 + ny) } }
+                    .flatten()
+                    .filterNot { it == this }
+                    .any { allPoints.contains(it) }
+        }
+    }
+
+    private data class Point(val x: Int, val y: Int, val dx: Int, val dy: Int) {
+        fun posAt(time: Int) = Pos(x + dx * time, y + dy * time)
+    }
+
+    private val initialState = parseInput(input)
+    private fun List<Point>.at(time: Int) = map { it.posAt(time) }.toSet()
+
+    private fun parseInput(input: List<String>): List<Point> {
+        return input.map {
+            // To parse: position=< 3,  6> velocity=<-1, -1>
+            Point(
+                    it.substringAfter("position=<").substringBefore(",").trim().toInt(),
+                    it.substringAfter(",").substringBefore(">").trim().toInt(),
+                    it.substringAfter("velocity=<").substringBefore(",").trim().toInt(),
+                    it.substringAfterLast(",").dropLast(1).trim().toInt()
+            )
+        }
+    }
+
+    private fun findMessage(): Int {
+        for (time in 1..100000) {
+            val currentArrangement = initialState.at(time)
+            if (currentArrangement.all { it.hasNeighbour(currentArrangement) }) {
+                return time
+            }
+        }
+        return -1
+    }
+
+    private fun printMessage(points: Set<Pos>) {
+        for (y in points.minBy { it.y }!!.y..points.maxBy { it.y }!!.y) {
+            for (x in points.minBy { it.x }!!.x..points.maxBy { it.x }!!.x) {
+                print(if (points.contains(Pos(x, y))) '#' else ' ')
+            }
+            println()
+        }
+    }
+
+    fun solvePart1(): Int {
+        val messageTime = findMessage()
+        printMessage(initialState.at(messageTime))
+        return messageTime
+    }
+
+    fun solvePart2(): Int {
+        return findMessage()
+    }
+}
