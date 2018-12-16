62d5981f38ed5d016b0f66f8b450973e7dd6b04f
Niklas Barsk <nibarius@gmail.com>
2018-12-16 23:29:26 +0100

    2018 Day 15: Beverage Bandits
    Hopefully this years most time consuming problem. Pretty fun at first
    but it got annyoing when it passes all provided tests but still fails
    with the real input. With some extra test cases from Reddit I managed
    to find the last few bugs and get my solution accepted.
    
    Part one ran in about 2 seconds to I just linearly checked all powers
    and found a solution in 1.5 minutes. I've spent enough time on this
    already to implement a binary search approach.
    
    Since there were so many shortest past problems during 2016 I decided
    to write a slightly more generic algorithm that hopefully can be
    reused in future problems if there are more shortest path problems.

---
+++ src/test/common/ShortestPathTest.kt
@@ -0,0 +1,81 @@
+import ShortestPath.Pos
+import org.junit.Assert
+import org.junit.Test
+
+class ShortestPathTest {
+    private fun List<String>.createMap(): Map<Pos, Char> {
+        val ret = mutableMapOf<Pos, Char>()
+        for (y in 0 until size) {
+            for (x in 0 until this[0].length) {
+                ret[Pos(x, y)] = this[y][x]
+            }
+        }
+        return ret
+    }
+
+    @Test
+    fun testOneStep() {
+        val map = listOf(
+                "####",
+                "#AB#",
+                "#..#",
+                "####").createMap()
+        val pathfinder = ShortestPath(listOf('.', 'A', 'B'))
+        // A -> B
+        Assert.assertEquals(listOf(Pos(1, 2)), pathfinder.find(map, Pos(1, 1), Pos(1, 2)))
+        // B -> A
+        Assert.assertEquals(listOf(Pos(1, 1)), pathfinder.find(map, Pos(2, 1), Pos(1, 1)))
+    }
+
+    @Test
+    fun testTwoSteps() {
+        val map = listOf(
+                "####",
+                "#A.#",
+                "#.B#",
+                "####").createMap()
+        val pathfinder = ShortestPath(listOf('.', 'A', 'B'))
+        // A -> B
+        Assert.assertEquals(listOf(Pos(2, 1), Pos(2, 2)), pathfinder.find(map, Pos(1, 1), Pos(2, 2)))
+        // B -> A
+        Assert.assertEquals(listOf(Pos(2, 1), Pos(1, 1)), pathfinder.find(map, Pos(2, 2), Pos(1, 1)))
+    }
+
+    @Test
+    fun testFourSteps() {
+        val map = listOf(
+                "#######",
+                "#.S...#",
+                "#.....#",
+                "#...G.#",
+                "#######").createMap()
+        val pathfinder = ShortestPath(listOf('.', 'S', 'G'))
+        Assert.assertEquals(listOf(Pos(3, 1), Pos(4, 1), Pos(4, 2), Pos(4, 3)), pathfinder.find(map, Pos(2, 1), Pos(4, 3)))
+    }
+
+    @Test
+    fun testBlockedPath() {
+        val map = listOf(
+                "#######",
+                "#.S...#",
+                "#.###.#",
+                "#...G##",
+                "#######").createMap()
+        val pathfinder = ShortestPath(listOf('.', 'S', 'G'))
+        val expected = listOf(Pos(1, 1), Pos(1, 2), Pos(1, 3),
+                Pos(2, 3), Pos(3,3), Pos(4, 3))
+        Assert.assertEquals(expected, pathfinder.find(map, Pos(2, 1), Pos(4, 3)))
+    }
+
+    @Test
+    fun testNoPath() {
+        val map = listOf(
+                "#######",
+                "#.S...#",
+                "#.###.#",
+                "#..#G##",
+                "#######").createMap()
+        val pathfinder = ShortestPath(listOf('.', 'S', 'G'))
+        Assert.assertEquals(listOf<Pos>(), pathfinder.find(map, Pos(2, 1), Pos(4, 3)))
+    }
+}
