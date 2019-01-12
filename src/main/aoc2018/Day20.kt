8627652ad3c543ba20b849f9c3370af1ed2e20c6
Niklas Barsk <nibarius@gmail.com>
2019-01-12 20:09:13 +0100

    Day 20: A Regular Map
    Quite easy when you used a good representation of the data,
    but very hard if you do things the wrong way.
    
    First attemt was with a strange kind of tree with strings as
    nodes (e.g. EEESSWW) that was completely wrong. Hard to write
    the code when the data types used couldn't represent the data
    in a correct way.
    
    Second attempt was to not parse the input character by character
    but instead with several passes and recursively parse each
    parenthesis and put the data in some kind of mix of a tree
    and a list. Parsing was slow, the code complicated and it missed
    rooms in dead ends (resulted in a off by two answer on my input).
    
    Third attempt was to go back to parsing the input character by
    character, but this time use a map to store the location and
    distance of each room. Code ended up very easy to read and write
    and runs quickly as well.

---
+++ src/main/aoc2018/Day20.kt
@@ -0,0 +1,54 @@
+package aoc2018
+
+import java.util.*
+
+class Day20(input: String) {
+
+    private data class Pos(val x: Int, val y: Int) {
+        fun move(dir: Char): Pos {
+            return when (dir) {
+                'N' -> Pos(x, y - 1)
+                'S' -> Pos(x, y + 1)
+                'W' -> Pos(x - 1, y)
+                'E' -> Pos(x + 1, y)
+                else -> throw IllegalArgumentException("Moving in invalid direction")
+            }
+        }
+    }
+
+    private data class Location(val pos: Pos, val steps: Int) {
+        fun move(dir: Char) = Location(pos.move(dir), steps + 1)
+    }
+
+    // Map of all rooms to the distance required to walk to them
+    private val map = makeMap(input.drop(1).dropLast(1))
+
+    // Doesn't support loops like ^NESW$, but my input doesn't have any loops like this.
+    // These are probably presented something like ^(NE|W)$ in the input which is supported.
+    private fun makeMap(input: String): Map<Pos, Int> {
+        val ret = mutableMapOf<Pos, Int>()
+        val branchPoints = ArrayDeque<Location>()
+        var current = Location(Pos(0, 0), 0)
+        for (char in input) {
+            when (char) {
+                'N', 'S', 'W', 'E' -> {
+                    current = current.move(char)
+                    ret.putIfAbsent(current.pos, current.steps)
+                }
+                '(' -> branchPoints.push(current)
+                '|' -> current = branchPoints.peek()
+                ')' -> current = branchPoints.pop()
+            }
+        }
+        return ret
+    }
+
+    fun solvePart1(): Int {
+        return map.values.max()!!
+    }
+
+
+    fun solvePart2(): Int {
+        return map.values.filter { it >= 1000 }.count()
+    }
+}
