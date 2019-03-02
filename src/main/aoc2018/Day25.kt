fa82bef11fac23f99acca682e0440e84235442d0
Niklas Barsk <nibarius@gmail.com>
2019-03-02 16:39:16 +0100

    2018: Day 25: Four-Dimensional Adventure
    Lesson of the day: You're not supposed to put things that change
    into a set, things will not end well if you change things inside a set.
    https://stackoverflow.com/questions/49646150/cant-remove-the-element-in-a-mutable-set-if-it-is-modified-in-kotlin
    https://try.kotlinlang.org/#/UserProjects/p19d1t2ntorbm04mi5su5crsb5/5n09890vcqtnv03gl6s4d8finb
    
    Except for that bug, things were pretyt easy.

--- src/main/aoc2018/Day25.kt
+++ src/main/aoc2018/Day25.kt
@@ -1,12 +1,42 @@
 package aoc2018
 
+import java.lang.Math.abs
+
 class Day25(input: List<String>) {
 
-    fun solvePart1(): Int {
-        return -1
+    data class Point(val coord: List<Int>) {
+        fun distanceTo(other: Point): Int {
+            return coord.zip(other.coord).map { abs(it.first - it.second) }.sum()
+        }
     }
 
-    fun solvePart2(): Int {
-        return -1
+    private val points = parseInput(input)
+
+    private fun parseInput(input: List<String>): Set<Point> {
+        val ret = mutableSetOf<Point>()
+        input.forEach { line ->
+            ret.add(Point(line.split(",").map { it.toInt() }))
+        }
+        return ret
+    }
+
+    private fun makeConstellations(points: Set<Point>): List<Set<Point>> {
+        val constellations = mutableListOf<MutableSet<Point>>()
+        points.forEach { point ->
+            val partOf = constellations.filter { it.map { it.distanceTo(point) }.any { it <= 3 } }
+            when {
+                partOf.isEmpty() -> constellations.add(mutableSetOf(point))
+                partOf.size == 1 -> partOf.first().add(point)
+                else -> {
+                    constellations.removeAll(partOf)
+                    constellations.add(partOf.flatten().toMutableSet().apply { add(point) })
+                }
+            }
+        }
+        return constellations.toList()
+    }
+
+    fun solvePart1(): Int {
+        return makeConstellations(points).size
     }
 }
