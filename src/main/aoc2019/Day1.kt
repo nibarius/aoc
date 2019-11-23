ae9eff8bd0cd725b393c5473f344bbca3d6b4162
Niklas Barsk <nibarius@gmail.com>
2019-11-23 14:13:14 +0100

    Prepare for 2019.
    Upgrade to latest version of Kotlin and Gradle.

---
+++ src/main/aoc2019/Day1.kt
@@ -0,0 +1,15 @@
+package aoc2019
+
+
+class Day1(input: List<String>) {
+
+    private val parsedInput = input.map { it.toInt() }
+
+    fun solvePart1(): Int {
+        return -1
+    }
+
+    fun solvePart2(): Int {
+        return -1
+    }
+}
