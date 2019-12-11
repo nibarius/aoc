8f6c37c3875feb3874585dd241c7177cbe861972
Niklas Barsk <nibarius@gmail.com>
2019-12-11 21:11:58 +0100

    2019 Day 10: Monitoring Station
    The math was the most difficult part here. In the first part I just calculated the angle
    as y/x just to see which asteroids had the same angle.
    
    Turned out that it didn't work well in part 2 where you had to iterate trough the
    asteroids based on smallest angle from straight up. Had to spend a lot of time
    googling on how trigonometry worked to refresh my memory and realize that
    it was some form of "tan" that should be used. Started out with atan and managed
    to get something fairly complicated that worked before I learned about atan2
    that was much more suitable.
    
    The final challenge was to rotate the coordinate system since the puzzle uses
    "y increases downwards" while normal trigonometry is the opposite.
    
    The rest of the puzzle wasn't very hard,  but there were of course some bugs that
    had to be solved before everything was working as it should.

---
+++ src/test/aoc2019/Day10Test.kt
@@ -0,0 +1,158 @@
+package test.aoc2019
+
+import aoc2019.Day10
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import resourceAsList
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.atan
+
+class Day10Test {
+    @Test
+    fun testPartOneExample1() {
+        val input = """
+              .#..#
+              .....
+              #####
+              ....#
+              ...##""".trimIndent().split("\n")
+        assertEquals(8, Day10(input).solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample2() {
+        val input = """
+            ......#.#.
+            #..#.#....
+            ..#######.
+            .#.#.###..
+            .#..#.....
+            ..#....#.#
+            #..#....#.
+            .##.#..###
+            ##...#..#.
+            .#....####
+        """.trimIndent().split("\n")
+        assertEquals(33, Day10(input).solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample3() {
+        val input = """
+            #.#...#.#.
+            .###....#.
+            .#....#...
+            ##.#.#.#.#
+            ....#.#.#.
+            .##..###.#
+            ..#...##..
+            ..##....##
+            ......#...
+            .####.###.""".trimIndent().split("\n")
+        assertEquals(35, Day10(input).solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample4() {
+        val input = """
+            .#..#..###
+            ####.###.#
+            ....###.#.
+            ..###.##.#
+            ##.##.#.#.
+            ....###..#
+            ..#.#..#.#
+            #..#.#.###
+            .##...##.#
+            .....#.#..""".trimIndent().split("\n")
+        assertEquals(41, Day10(input).solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample5() {
+        val input = """
+            .#..##.###...#######
+            ##.############..##.
+            .#.######.########.#
+            .###.#######.####.#.
+            #####.##.#.##.###.##
+            ..#####..#.#########
+            ####################
+            #.####....###.#.#.##
+            ##.#################
+            #####.##.###..####..
+            ..######..##.#######
+            ####.##.####...##..#
+            .#####..#.######.###
+            ##...#.##########...
+            #.##########.#######
+            .####.#.###.###.#.##
+            ....##.##.###..#####
+            .#.#.###########.###
+            #.#.#.#####.####.###
+            ###.##.####.##.#..##""".trimIndent().split("\n")
+        assertEquals(210, Day10(input).solvePart1())
+    }
+
+    @Test
+    fun testMagic() {
+        val self = Day10.Pos(5, 5)
+        val up = Day10.Pos(5, 0)
+        val right = Day10.Pos(10, 5)
+        val down = Day10.Pos(5, 10)
+        val left = Day10.Pos(0, 5)
+        assertEquals(0.0, Day10.Angle.calculate(self, up).value, 0.01)
+        assertEquals(-PI / 2, Day10.Angle.calculate(self, right).value, 0.01)
+        assertEquals(-PI, Day10.Angle.calculate(self, down).value, 0.01)
+        assertEquals(-(PI * 1.5), Day10.Angle.calculate(self, left).value, 0.01)
+    }
+
+    @Test
+    fun partOneRealInput() {
+        assertEquals(282, Day10(resourceAsList("2019/day10.txt")).solvePart1())
+    }
+
+    // Only for debugging, doesn't return any meaningful value
+    @Test
+    fun testPartTwoPartialExample() {
+        val input = """
+            .#....#####...#..
+            ##...##.#####..##
+            ##...#...#.#####.
+            ..#.....#...###..
+            ..#.#.....#....##""".trimIndent().split("\n")
+        assertEquals(-1, Day10(input).solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample() {
+        val input = """
+            .#..##.###...#######
+            ##.############..##.
+            .#.######.########.#
+            .###.#######.####.#.
+            #####.##.#.##.###.##
+            ..#####..#.#########
+            ####################
+            #.####....###.#.#.##
+            ##.#################
+            #####.##.###..####..
+            ..######..##.#######
+            ####.##.####...##..#
+            .#####..#.######.###
+            ##...#.##########...
+            #.##########.#######
+            .####.#.###.###.#.##
+            ....##.##.###..#####
+            .#.#.###########.###
+            #.#.#.#####.####.###
+            ###.##.####.##.#..##""".trimIndent().split("\n")
+        assertEquals(802, Day10(input).solvePart2())
+    }
+
+    @Test
+    fun partTwoRealInput() {
+        assertEquals(1008, Day10(resourceAsList("2019/day10.txt")).solvePart2())
+    }
+}
