f65cd4a24e049b06ccfb34ca0d0d09788922a2c8
Niklas Barsk <nibarius@gmail.com>
2019-12-11 21:11:58 +0100

    2019 Day 10: Monitoring Station
    The math was the most difficult part here. In the first part I just calculated the angle
    as y/x just to see which asteroids had the same angle.
    
    Turned out that it didn't work well in part 2 where you had to iterate trough the
    asteroids based on smallest angle from straight up. Had to spend a lot of time
    googling on how trigonometry worked to refresh my memory and realize that
    it was some form of "tan" that should be used. Started out with atan and managed
    to get something fairly complicated that worked before I learned about atan2
    that was much more suitable.
    
    The final challenge was to rotate the coordinate system since the puzzle uses
    "y increases downwards" while normal trigonometry is the opposite.
    
    The rest of the puzzle wasn't very hard,  but there were of course some bugs that
    had to be solved before everything was working as it should.

---
+++ src/main/aoc2019/Day10.kt
@@ -0,0 +1,74 @@
+package aoc2019
+
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.atan2
+
+class Day10(input: List<String>) {
+    data class Pos(val x: Int, val y: Int)
+
+    data class Angle(val value: Double) {
+        var pos = Pos(0, 0)
+        var distance: Int = 0
+
+        companion object {
+            fun calculate(self: Pos, other: Pos): Angle {
+                // Atan2 - PI gives angle 0 for straight up and negative angles for everything else
+                // with angle decreasing further while rotating clockwise
+                return Angle(atan2((other.x - self.x).toDouble(), (other.y - self.y).toDouble()) - PI)
+                        .apply {
+                            distance = abs((other.x - self.x)) + abs((other.y - self.y))
+                            pos = other
+                        }
+            }
+        }
+    }
+
+    private fun List<Pos>.inSightOf(other: Pos): Int {
+        return this.filterNot { it == other }
+                .map { Angle.calculate(other, it) }
+                .toSet()
+                .size
+    }
+
+    private val asteroids = parseInput(input)
+
+    private fun parseInput(input: List<String>): List<Pos> {
+        return mutableListOf<Pos>().apply {
+            input.forEachIndexed { y, s ->
+                s.forEachIndexed { x, c -> if (c == '#') this.add(Pos(x, y)) }
+            }
+        }
+    }
+
+    fun solvePart1(): Int {
+        return asteroids.map { asteroids.inSightOf(it) }.max() ?: 0
+    }
+
+    fun solvePart2(): Int {
+        val best = asteroids.map { it to asteroids.inSightOf(it) }.maxBy { it.second }!!.first
+        val targets: MutableMap<Double, MutableList<Angle>> = asteroids.filterNot { it == best }
+                .map { Angle.calculate(best, it) }
+                // smallest angle (from up) first, then smallest distance
+                .sortedWith(compareBy({ abs(it.value) }, { it.distance }))
+                .groupByTo(mutableMapOf()) { it.value }
+
+        var key: Double = 1.0
+        var lastKill = best
+        repeat(200) {
+            // Keep moving clockwise to next angle each time, or wrap around if needed
+            key = targets.keys.firstOrNull { it < key } ?: targets.keys.first()
+            lastKill = targets[key]!!.first().pos
+            if (targets[key]!!.size <= 1) { // No other asteroids hides behind this one
+                targets.remove(key)
+            } else { // Other asteroids hides behind this, only destroy the first one
+                targets[key]!!.removeAt(0)
+            }
+            if (targets.isEmpty()) { // Incomplete test case only
+                return -1
+            }
+        }
+
+        return lastKill.x * 100 + lastKill.y
+    }
+}
