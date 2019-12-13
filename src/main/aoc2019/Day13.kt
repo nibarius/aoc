43f928396b2af8e51c1270d28f0e984ae74334b5
Niklas Barsk <nibarius@gmail.com>
2019-12-13 23:44:57 +0100

    Add some code that prints output to png files to be able to see the game

--- src/main/aoc2019/Day13.kt
+++ src/main/aoc2019/Day13.kt
@@ -1,5 +1,10 @@
 package aoc2019
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
 class Day13(input: List<String>) {
     var parsedInput = input.map { it.toLong() }
 
@@ -19,11 +24,15 @@ class Day13(input: List<String>) {
         fun countBlocks() = screen.values.count { it == BLOCK }
 
         fun play(): Long {
+            var iter = 0
             do {
                 computer.run()
                 updateScreen()
                 computer.input.add((ballX - paddleX).coerceIn(-1, 1))
+
+                //drawStuff(screen, iter++)
             } while (computer.computerState != Intcode.ComputerState.Terminated)
+
             return score
         }
 
@@ -37,6 +46,33 @@ class Day13(input: List<String>) {
                 screen[Pair(x, y)] = type
             }
             computer.output.clear()
+        }
+
+        private fun Map<Pair<Long, Long>, Long>.xRange() = keys.minBy { it.first }!!.first..keys.maxBy { it.first }!!.first
+        private fun Map<Pair<Long, Long>, Long>.yRange() = keys.minBy { it.second }!!.second..keys.maxBy { it.second }!!.second
+
+        fun drawStuff(toDraw: Map<Pair<Long, Long>, Long>, frame: Int) {
+            val yrange = toDraw.yRange()
+            val xrange = toDraw.xRange()
+            val img = BufferedImage(xrange.count(), yrange.count(), BufferedImage.TYPE_INT_RGB)
+            (yrange).forEach { y ->
+                (xrange).forEach { x ->
+                    if (x >= 0) {
+                        toDraw.getOrDefault(Pair(x, y), -1).let { color ->
+                            val realColor = when (color) {
+                                0L -> Color.BLACK.rgb
+                                1L -> Color.BLUE.rgb
+                                2L -> Color.GREEN.rgb
+                                3L -> Color.RED.rgb
+                                4L -> Color.WHITE.rgb
+                                else -> Color.BLACK.rgb
+                            }
+                            img.setRGB(x.toInt(), y.toInt(), realColor)
+                        }
+                    }
+                }
+            }
+            ImageIO.write(img, "png", File("img/frame${frame.toString().padStart(4, '0')}.png"))
         }
     }
 
