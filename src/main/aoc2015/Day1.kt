1996399880359003f6ad527226c40ffd40e865e6
Niklas Barsk <nibarius@gmail.com>
2020-02-18 22:31:31 +0100

    2015 Day 1: Not Quite Lisp

--- src/main/aoc2015/Day1.kt
+++ src/main/aoc2015/Day1.kt
@@ -1,12 +1,17 @@
 package aoc2015
 
-class Day1(input: List<String>) {
+class Day1(val input: String) {
 
     fun solvePart1(): Int {
-        return -1
+        return input.fold(0) { total, char -> total + if (char == '(') 1 else -1 }
     }
 
     fun solvePart2(): Int {
+        var total = 0
+        input.withIndex().forEach { (index, char) ->
+            if (char == '(') total++ else total--
+            if (total == -1) return index + 1
+        }
         return -1
     }
 }
--- src/test/aoc2015/Day1Test.kt
+++ src/test/aoc2015/Day1Test.kt
@@ -4,27 +4,51 @@ import aoc2015.Day1
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import resourceAsList
+import resourceAsString
 
 class Day1Test {
     @Test
     fun testPartOneExample1() {
-        val input = """""".trimIndent().split("\n")
-        assertEquals(0, Day1(input).solvePart1())
+        assertEquals(0, Day1("(())").solvePart1())
+        assertEquals(0, Day1("()()").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample2() {
+        assertEquals(3, Day1("(((").solvePart1())
+        assertEquals(3, Day1("(()(()(").solvePart1())
+        assertEquals(3, Day1("))(((((").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample3() {
+        assertEquals(-1, Day1("())").solvePart1())
+        assertEquals(-1, Day1("))(").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample4() {
+        assertEquals(-3, Day1(")))").solvePart1())
+        assertEquals(-3, Day1(")())())").solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
-        assertEquals(0, Day1(resourceAsList("2015/day1.txt")).solvePart1())
+        assertEquals(74, Day1(resourceAsString("2015/day1.txt")).solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
-        val input = """""".trimIndent().split("\n")
-        assertEquals(0, Day1(input).solvePart2())
+        assertEquals(1, Day1(")").solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample2() {
+        assertEquals(5, Day1("()())").solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
-        assertEquals(0, Day1(resourceAsList("2015/day1.txt")).solvePart2())
+        assertEquals(1795, Day1(resourceAsString("2015/day1.txt")).solvePart2())
     }
 }
