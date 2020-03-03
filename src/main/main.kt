2ef4b4764cee767e2ac5e6ef55f556285b914eee
Niklas Barsk <nibarius@gmail.com>
2020-03-03 21:45:46 +0100

    2015 Day 20: Infinite Elves and Infinite Houses This was much harder than it had to be.
    First I tried to calculate the number of presents for each house
    by summing all factors, but that took for ever. Then I spent a lot
    of time trying to figure out a way to calculate some lower or
    upper bound close to the target to be able to calculate number of
    presents from there. But that wasn't really possible either.
    
    Turned out that just simulating the whole thing by having 100k elves
    drop presents to 100k houses and finding the first house that matches
    the criteria was very quick and easy.

--- src/main/main.kt
+++ src/main/main.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main() {
     val year = 2015
-    val day = 20
+    val day = 21
     createDayClassFile(year, day)
     createTestFile(year, day)
     createInputFile("$year/day$day.txt")
---
+++ src/test/aoc2015/Day20Test.kt
@@ -0,0 +1,20 @@
+package test.aoc2015
+
+import aoc2015.Day20
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import resourceAsList
+import resourceAsString
+
+class Day20Test {
+
+    @Test
+    fun partOneRealInput() {
+        assertEquals(831600, Day20(resourceAsString("2015/day20.txt")).solvePart1())
+    }
+
+    @Test
+    fun partTwoRealInput() {
+        assertEquals(0, Day20(resourceAsString("2015/day20.txt")).solvePart2())
+    }
+}
