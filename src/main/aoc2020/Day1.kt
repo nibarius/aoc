77eaf2b61b82400696c15adc083fc8fc20dfb0ea
Niklas Barsk <nibarius@gmail.com>
2020-12-01 21:57:33 +0100

    2020 Day 1: Report Repair
    As expected an easy challenge, so took the opportunity to optimize the solution
    a bit. Got total runtime down from 40-60ms to 1ms.
    
    Optimizations done:
     - Since the class is completely statelss, create only one class per
       input and only convert the input to the desired form once.
     - The input contains mostly entries that when summed will be too large
       so sorting the list and starting from the smaller numbers helps a lot.
     - Missed it myself, but learned from other solutions that you can
       keep a set of already seen numbers to not have to iterate trough
       the list one last time for the final digit. Very helpful.

--- src/main/aoc2020/Day1.kt
+++ src/main/aoc2020/Day1.kt
@@ -1,12 +1,43 @@
 package aoc2020
 
 class Day1(input: List<String>) {
+    // Most of the entries in the input will go past 2020 when summed
+    // so sort the list to start with smaller numbers to find the solution faster
+    val entries = input.map { it.toInt() }.sorted()
+
+
+    fun solve(partOne: Boolean = false, partTwo: Boolean = false): Int {
+        // Keep a set of already seen numbers to save one for loop and reduce complexity
+        // from O(n^2) to O(n) in part 1 and O(n^3) to O(n^2) in part 2
+        val seen = mutableSetOf<Int>()
+        for (x in entries.indices) {
+            if (partOne) {
+                seen.add(entries[x])
+                val lookingFor = 2020 - entries[x]
+                if (seen.contains(lookingFor)) {
+                    return entries[x] * lookingFor
+                }
+            }
+            if (partTwo) {
+                for (y in entries.indices) {
+                    if (x != y) {
+                        seen.add(entries[y])
+                        val lookingFor = 2020 - entries[x] - entries[y]
+                        if (seen.contains(lookingFor)) {
+                            return entries[x] * entries[y] * lookingFor
+                        }
+                    }
+                }
+            }
+        }
+        return 0
+    }
 
     fun solvePart1(): Int {
-        return -1
+        return solve(partOne = true)
     }
 
     fun solvePart2(): Int {
-        return -1
+        return solve(partTwo = true)
     }
 }
