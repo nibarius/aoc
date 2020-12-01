a2ac75e6923dbf0f4138a4692745fc6dc8ee84b1
Niklas Barsk <nibarius@gmail.com>
2020-12-01 21:57:33 +0100

    2020 Day 1: Report Repair
    As expected an easy challenge, so took the opportunity to optimize the solution
    a bit. Got total runtime down from 40-60ms to 1ms.
    
    Optimizations done:
     - Since the class is completely statelss, create only one class per
       input and only convert the input to the desired form once.
     - The input contains mostly entries that when summed will be too large
       so sorting the list and starting from the smaller numbers helps a lot.
     - Missed it myself, but learned from other solutions that you can
       keep a set of already seen numbers to not have to iterate trough
       the list one last time for the final digit. Very helpful.

--- src/test/aoc2020/Day1Test.kt
+++ src/test/aoc2020/Day1Test.kt
@@ -6,25 +6,35 @@ import org.junit.Test
 import resourceAsList
 
 class Day1Test {
+    private val example = """
+            1721
+            979
+            366
+            299
+            675
+            1456
+        """.trimIndent().split("\n")
+
+    private val day1 = Day1(resourceAsList("2020/day1.txt"))
+    private val day1Example = Day1(example)
+
     @Test
     fun testPartOneExample1() {
-        val input = """""".trimIndent().split("\n")
-        assertEquals(0, Day1(input).solvePart1())
+        assertEquals(514579, day1Example.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
-        assertEquals(0, Day1(resourceAsList("2020/day1.txt")).solvePart1())
+        assertEquals(437931, day1.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
-        val input = """""".trimIndent().split("\n")
-        assertEquals(0, Day1(input).solvePart2())
+        assertEquals(241861950, day1Example.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
-        assertEquals(0, Day1(resourceAsList("2020/day1.txt")).solvePart2())
+        assertEquals(157667328, day1.solvePart2())
     }
 }
