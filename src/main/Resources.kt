584cbe7ec3a8d6b984992b8475e5e8346b1ca0ff
Niklas Barsk <nibarius@gmail.com>
2020-12-06 20:47:16 +0100

    2020 Day 6: Custom Customs
    Finding an initial solution was fairly quick. But after two days with problems
    where different entries in the input is separated by double linebreaks I
    decided to make a general approach to that. So I'm introducing a new method
    for reading input and splitting it on double linebreaks instead of single.

--- src/main/Resources.kt
+++ src/main/Resources.kt
@@ -1,18 +1,40 @@
 import java.io.File
 
 internal object Resources
+
+/**
+ * Read a file and return the content as a string where each line is
+ * separated with the given delimiter.
+ */
 fun resourceAsString(fileName: String, delimiter: String = ""): String {
-    return File(Resources.javaClass.classLoader.getResource(fileName).toURI())
+    return File(Resources.javaClass.classLoader.getResource(fileName)!!.toURI())
             .readLines()
             .reduce { a, b -> "$a$delimiter$b" }
 }
 
+/**
+ * Read the given file and return the content as a list of strings, one for each line.
+ * Note: the last trailing blank line is ignored.
+ */
 fun resourceAsList(fileName: String): List<String> =
-        File(Resources.javaClass.classLoader.getResource(fileName).toURI())
+        File(Resources.javaClass.classLoader.getResource(fileName)!!.toURI())
                 .readLines()
 
+/**
+ * Reads the first line of text in the given file and split it on the given delimiter.
+ */
 fun resourceAsList(fileName: String, delimiter: String): List<String> =
-        File(Resources.javaClass.classLoader.getResource(fileName).toURI())
+        File(Resources.javaClass.classLoader.getResource(fileName)!!.toURI())
                 .readLines()
                 .first()
                 .split(delimiter)
+
+/**
+ * Reads the given file and first split it on empty lines (two consecutive newlines).
+ * Any single newlines in each string is represented as \n regardless of System line endings.
+ */
+fun resourceSplitOnBlankLines(fileName: String): List<String> {
+    return resourceAsList(fileName)
+            .joinToString("\n")
+            .split("\n\n")
+}
---
+++ src/main/aoc2020/Day6.kt
@@ -0,0 +1,20 @@
+package aoc2020
+
+// Input is the input file split on \n\n (each group of passengers).
+// So each entry in the list is one group. Within each group the passengers are separated by \n.
+class Day6(val input: List<String>) {
+    fun solvePart1(): Int {
+        return input.sumBy { group ->
+            group.toSet().filter { it.isLetter() }.size
+        }
+    }
+
+    fun solvePart2(): Int {
+        return input.sumBy {
+            it.split("\n")
+                    .map { person -> person.toSet() }
+                    .reduce { acc, person -> acc intersect person }
+                    .size
+        }
+    }
+}
