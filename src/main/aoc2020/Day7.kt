122236b7bd30354b7d694ecc2d3b421049675ad9
Niklas Barsk <nibarius@gmail.com>
2020-12-07 21:44:54 +0100

    2020 Day 7: Handy Haversacks
    Fairly easy to understand roughtly what to do, but a bit more tricky
    to get it working. And then after that getting it nice and understandable.
    
    The parsing part still feels a bit messy to me.

--- src/main/aoc2020/Day7.kt
+++ src/main/aoc2020/Day7.kt
@@ -1,12 +1,69 @@
 package aoc2020
 
+@Suppress("MapGetWithNotNullAssertionOperator")
 class Day7(input: List<String>) {
 
+    private data class Node(val parents: MutableSet<String> = mutableSetOf(), val children: MutableMap<String, Int> = mutableMapOf())
+
+    private val myBag = "shiny gold"
+    private val bags = parseInput(input)
+
+    private fun MutableMap<String, Node>.addChildToParent(child: String, parent: String) {
+        if (!contains(child)) this[child] = Node()
+        this[child]!!.parents.add(parent)
+    }
+
+    // Example input:
+    // light red bags contain 1 bright white bag, 2 muted yellow bags.
+    // faded blue bags contain no other bags.
+    private fun parseInput(input: List<String>): Map<String, Node> {
+        val ret = mutableMapOf<String, Node>()
+        for (line in input) {
+            val (name, contents) = line.split(" bags contain ")
+
+            // Create entry for this node if needed
+            if (!ret.contains(name)) ret[name] = Node()
+            if (contents == "no other bags.") continue
+
+            // Parse and set all children for this node
+            contents.split(", ")
+                    .map { bag -> // each entry becomes a bag name to quantity pair
+                        "([0-9]+) (.*) bag.{0,2}".toRegex().matchEntire(bag)!!
+                                .destructured
+                                .let { (qty, name) -> Pair(name, qty.toInt()) }
+                    }
+                    .toMap()
+                    // add all children into ret
+                    .apply { ret[name]!!.children.putAll(this) }
+                    // mark this node as parent for all its children
+                    .also { it.keys.forEach { child -> ret.addChildToParent(child, name) } }
+
+        }
+        return ret
+    }
+
+    private fun MutableSet<String>.addAllAncestorsOf(name: String) {
+        // the bag itself + all its parents
+        add(name)
+        bags[name]!!.parents
+                .forEach { parent -> addAllAncestorsOf(parent) }
+    }
+
+    private fun countBagsContainedBy(name: String): Int {
+        // the bag itself + all its children
+        return 1 + bags[name]!!.children
+                .toList()
+                .sumBy { it.second * countBagsContainedBy(it.first) }
+
+    }
+
     fun solvePart1(): Int {
-        return -1
+        return mutableSetOf<String>()
+                .apply { addAllAncestorsOf(myBag) }
+                .size - 1 // subtract one since start bag should not be counted
     }
 
     fun solvePart2(): Int {
-        return -1
+        return countBagsContainedBy(myBag) - 1 // subtract one since start bag should not be counted
     }
 }
--- src/test/aoc2020/Day7Test.kt
+++ src/test/aoc2020/Day7Test.kt
@@ -6,29 +6,39 @@ import org.junit.Test
 import resourceAsList
 
 class Day7Test {
-    private val exampleInput = """""".trimIndent().split("\n")
+    private val exampleInput = """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+    """.trimIndent().split("\n")
 
     @Test
     fun testPartOneExample1() {
         val day7 = Day7(exampleInput)
-        assertEquals(0, day7.solvePart1())
+        assertEquals(4, day7.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
         val day7 = Day7(resourceAsList("2020/day7.txt"))
-        assertEquals(0, day7.solvePart1())
+        assertEquals(254, day7.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
         val day7 = Day7(exampleInput)
-        assertEquals(0, day7.solvePart2())
+        assertEquals(32, day7.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
         val day7 = Day7(resourceAsList("2020/day7.txt"))
-        assertEquals(0, day7.solvePart2())
+        assertEquals(6006, day7.solvePart2())
     }
 }
