566cd2be18fb0bd3e3d25036bdab4ea8e36ba992
Niklas Barsk <nibarius@gmail.com>
2020-12-11 19:50:45 +0100

    2020 Day 11: Seating System
    Fairly easy, biggest effort was extending the AMap class
    to support looking beyond the nearest neighbour (the
    previous commit).

--- src/main/main.kt
+++ src/main/main.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val year = 2020
-    val day = 10
+    val day = 11
     if(args.firstOrNull() == "download") {
         readInputFileFromInternet(year, day)
     }
