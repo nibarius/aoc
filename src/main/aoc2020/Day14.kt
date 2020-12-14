18864e1ec3c1cbb019ffc51cd2174700b2735bf2
Niklas Barsk <nibarius@gmail.com>
2020-12-14 19:28:56 +0100

    2020 Day 14: Docking Data
    Part one was easy while part two was a bit more tricky. It was
    easy to know what had to be done, but I had to think a bit to
    figure out how to implement the generation of multiple addresses
    at once.

---
+++ src/main/aoc2020/Day14.kt
@@ -0,0 +1,71 @@
+package aoc2020
+
+class Day14(input: List<String>) {
+    sealed class Instruction {
+        data class Mask(val mask: String) : Instruction()
+        data class Write(val address: String, val value: String) : Instruction()
+    }
+
+    val instructions = input.map { line ->
+        when {
+            line.startsWith("mask") -> Instruction.Mask(line.substringAfter("mask = "))
+            line.startsWith("mem") -> {
+                val (address, value) = "mem\\[(\\d+)] = (\\d+)".toRegex().find(line)!!.destructured
+                Instruction.Write(
+                        address.toInt().toString(2).padStart(36, '0'),
+                        value.toInt().toString(2).padStart(36, '0')
+                )
+            }
+            else -> error("unknown input: $line")
+        }
+    }
+
+    private fun String.applyMask(mask: String, maskIgnores: Char): String {
+        return CharArray(36) { if (mask[it] == maskIgnores) this[it] else mask[it] }.joinToString("")
+    }
+
+    // Write one masked value to one address
+    private fun writeOne(memory: MutableMap<String, String>, instruction: Instruction.Write, mask: String) {
+        memory[instruction.address] = instruction.value.applyMask(mask, 'X')
+    }
+
+    // Write one value to all addresses matching the masked address
+    private fun writeAll(memory: MutableMap<String, String>, instruction: Instruction.Write, mask: String) {
+        val address = instruction.address.applyMask(mask, '0')
+        val ret = mutableListOf(CharArray(36))
+        for (i in 0 until 36) {
+            if (address[i] == 'X') {
+                val copy = ret.deepCopy()
+                ret.forEach { it[i] = '0' }
+                copy.forEach { it[i] = '1' }
+                ret.addAll(copy)
+            } else {
+                ret.forEach { it[i] = address[i] }
+            }
+        }
+        ret.forEach { memory[it.joinToString("")] = instruction.value }
+    }
+
+    private fun List<CharArray>.deepCopy(): MutableList<CharArray> =
+            mutableListOf<CharArray>().apply { this@deepCopy.forEach { add(it.clone()) } }
+
+    private fun run(writeFn: (MutableMap<String, String>, Instruction.Write, String) -> Unit): Long {
+        val memory = mutableMapOf<String, String>()
+        var mask = ""
+        instructions.forEach { instruction ->
+            when (instruction) {
+                is Instruction.Mask -> mask = instruction.mask
+                is Instruction.Write -> writeFn(memory, instruction, mask)
+            }
+        }
+        return memory.values.map { it.toLong(2) }.sum()
+    }
+
+    fun solvePart1(): Long {
+        return run(this::writeOne)
+    }
+
+    fun solvePart2(): Long {
+        return run(this::writeAll)
+    }
+}
