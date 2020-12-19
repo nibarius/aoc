5bf05ba07490b19c96050ec3d54c4626701e8fac
Niklas Barsk <nibarius@gmail.com>
2020-12-19 21:14:42 +0100

    2020 Day 19: Monster Messages (part 1)
    I struggled a lot with day 18 and this felt very similar. Figuring out
    how to do part 1 took forever. Tried various approaches of parsing
    the data but everything ended up in disaster until I finally came
    to this solution.
    
    It does not work for part two though and I don't know why it doesn't
    work. There is no infinite recursion problems as I expected since
    loops were introduced, it just provides the wrong answer. But I
    haven't been able to figure out why.
    
    I also don't understand the hint at all:
    
       Fortunately, many of the rules are unaffected by this change;
       it might help to start by looking at which rules always match
       the same set of values and how those rules (especially rules 42
       and 31) are used by the new versions of rules 8 and 11.
    
    Sounds like you're expected to somehow generate output based on
    the rules rather than validating the strings using the rules,
    in which case my part 1 solution won't help at all.
    
    I'll have to come back to this and try to solve part 2 at a later
    time.

--- src/main/main.kt
+++ src/main/main.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val year = 2020
-    val day = 17
+    val day = 19
     if(args.firstOrNull() == "download") {
         readInputFileFromInternet(year, day)
     }
