bdcb7c830a8c9e768075e4a7e6eeb678576f7630
Niklas Barsk <nibarius@gmail.com>
2020-12-20 23:09:48 +0100

    2020 Day 20: Jurassic Jigsaw
    This was much more fun than the last two days. It was much more
    obvious what needed to be done and how to do it.
    
    But there was a lot of code to write and some thinking to do
    and of course several bugs to track down so it took quite some
    time to get everything working.
    
    Still needs cleanup and improvements, but I'll save that for
    another day.

---
+++ src/test/common/GridTest.kt
@@ -0,0 +1,91 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GridTest {
+    @Test
+    fun testRotateLeft() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val expected = """
+                3cC
+                2bB
+                1aA
+            """.trimIndent()
+        assertEquals(expected, grid.toString(listOf(Grid.Transformation.RotateLeft)))
+    }
+
+    @Test
+    fun testRotateRight() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val expected = """
+            Aa1
+            Bb2
+            Cc3
+        """.trimIndent()
+        assertEquals(expected, grid.toString(listOf(Grid.Transformation.RotateRight)))
+    }
+
+    @Test
+    fun testRotate180() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val expected = """
+            CBA
+            cba
+            321
+        """.trimIndent()
+        assertEquals(expected, grid.toString(listOf(Grid.Transformation.Rotate180)))
+    }
+
+    @Test
+    fun testFlipHorizontal() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val expected = """
+            321
+            cba
+            CBA
+        """.trimIndent()
+        assertEquals(expected, grid.toString(listOf(Grid.Transformation.FlipHorizontal)))
+    }
+
+    @Test
+    fun testFlipVertical() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val expected = """
+            ABC
+            abc
+            123
+        """.trimIndent()
+        assertEquals(expected, grid.toString(listOf(Grid.Transformation.FlipVertical)))
+    }
+
+    @Test
+    fun testAllPossibleTransformations() {
+        val grid = Grid.parse("""
+            123
+            abc
+            ABC
+        """.trimIndent().split("\n"))
+        val all = grid.possibleTransformations.map { grid.toString(it) to it }.sortedBy { it.first }
+        assertEquals(8, all.size)
+        assertEquals(all.size, all.map { it.first }.toSet().size)
+    }
+}
