fc2cca9d94c66b33e49c23a77c7e9c52f0cb77d2
Niklas Barsk <nibarius@gmail.com>
2020-12-26 21:03:40 +0100

    Use the generic xRange/yRange methods in AMap and 2019 day 11

--- src/main/AMap.kt
+++ src/main/AMap.kt
@@ -54,9 +54,6 @@ class AMap(private val map: MutableMap<Pos, Char> = mutableMapOf()) {
         toString(emptySpace).split('\n').forEach { println(it) }
     }
 
-    private fun Map<Pos, Char>.xRange() = keys.minByOrNull { it.x }!!.x..keys.maxByOrNull { it.x }!!.x
-    private fun Map<Pos, Char>.yRange() = keys.minByOrNull { it.y }!!.y..keys.maxByOrNull { it.y }!!.y
-
     companion object {
         fun parse(input: List<String>): AMap {
             val map = AMap()
