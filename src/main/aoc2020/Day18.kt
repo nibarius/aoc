43555596a9ca3ab22ae9d343846e2af8330c5799
Niklas Barsk <nibarius@gmail.com>
2021-01-01 21:20:15 +0100

    Improved solution for 2020 day 18
    Cleaned up the original solution and replaced part 1 with a more
    generic version of part 2 that can solve both versions.
    
    To keep the code a bit cleaner several casts are done and assumptions on
    which Expressions shows up where. But the input provided is well formed
    so this can be safely done. Also leads to execptions being thrown if
    bugs are introduced and unexpected types ends up at the wrong place.
    
    Probably still not the nicest solution, but much better than the
    first one.

--- src/main/aoc2020/Day18.kt
+++ src/main/aoc2020/Day18.kt
@@ -1,119 +1,100 @@
 package aoc2020
 
+import java.lang.Integer.max
+
 class Day18(input: List<String>) {
-    val problems = input.map { line -> line.mapNotNull { ch -> ch.takeUnless { it == ' ' } } }
+    val problems = input
+            .map { line -> line.mapNotNull { ch -> ch.takeUnless { it == ' ' } } }
+            .map { parseInput(it) }
 
-    private fun handleNumber(left: MutableList<Long?>, operator: MutableList<(Long, Long) -> Long>, number: Long) {
-        if (left.last() == null) {
-            left[left.size - 1] = number
-        } else {
-            val op = operator.removeLast()
-            left[left.size - 1] = op.invoke(left.last()!!, number)
-        }
-    }
-
-    private fun calculate(problem: List<Char>): Long {
-
-        val operator = mutableListOf<(Long, Long) -> Long>()
-        val left = mutableListOf<Long?>(null)
-
-        for (token in problem) {
-            when (token) {
-                '*' -> operator.add(Long::times)
-                '+' -> operator.add(Long::plus)
-                '(' -> left.add(null)
-                ')' -> {
-                    val number = left.removeLast()!!
-                    if (operator.isEmpty()) {
-                        require(left.last() == null)
-                        left[left.size - 1] = number
-                    } else {
-                        handleNumber(left, operator, number)
-                    }
-                }
-                else -> {
-                    val number = token.toString().toLong()
-                    handleNumber(left, operator, number)
+    sealed class Expression {
+        data class Operator(private val op: Char) : Expression() {  // *, +
+            fun calculate(lValue: Expression, rValue: Expression): Long {
+                lValue as Literal; rValue as Literal
+                return when (op) {
+                    '+' -> lValue.value + rValue.value
+                    '*' -> lValue.value * rValue.value
+                    else -> error("unknown operator")
                 }
             }
         }
-        return left.single()!!
-    }
 
-    fun solvePart1(): Long {
-        return problems.sumOf { calculate(it) }
-    }
-
-    sealed class Expression {
-        data class Operator(val op: Char) : Expression()
-        data class Literal(val value: Long) : Expression()
-        data class Complex(val list: MutableList<Expression>) : Expression()
+        data class Literal(val value: Long) : Expression() // numbers
+        data class Complex(val list: MutableList<Expression>) : Expression() // The root problem / inside a parenthesis
     }
 
     private fun parseInput(problem: List<Char>): Expression {
-        val ret = mutableListOf<Expression>()
-        val expressionInProgress = mutableListOf<Expression.Complex>()
+        // List of complex expressions, where the first entry is the root expression and
+        // the rest each yet unfinished parenthesis
+        val expression = mutableListOf(Expression.Complex(mutableListOf()))
         for (token in problem) {
             when (token) {
-                '(' -> {
-                    expressionInProgress.add(Expression.Complex(mutableListOf()))
+                '(' -> { // Start a new complex expression
+                    expression.add(Expression.Complex(mutableListOf()))
                 }
-                ')' -> {
-                    val finishedExpression = expressionInProgress.removeLast()
-                    if (expressionInProgress.isNotEmpty()) {
-                        expressionInProgress.last().list.add(finishedExpression)
-                    } else {
-                        ret.add(finishedExpression)
-                    }
+                ')' -> { // Finalize the latest complex expression
+                    val finishedExpression = expression.removeLast()
+                    expression.last().list.add(finishedExpression)
                 }
-                else -> {
-                    val toAdd = if(token in listOf('+', '*')) Expression.Operator(token) else Expression.Literal(token.toString().toLong())
-                    if (expressionInProgress.isNotEmpty()) {
-                        expressionInProgress.last().list.add(toAdd)
+                else -> { // Add the operator or literal to the current expression
+                    val toAdd = if (token in listOf('+', '*')) {
+                        Expression.Operator(token)
                     } else {
-                        ret.add(toAdd)
+                        Expression.Literal(token.toString().toLong())
                     }
+                    expression.last().list.add(toAdd)
                 }
             }
         }
-        return Expression.Complex(ret)
+        return expression.first()
     }
 
-    private fun calculate2(expression: Expression): Expression {
-        //2 * 3 + (4 * 5)
-        if (expression is Expression.Literal) {
-            return expression // case for (4)
+    /**
+     * Calculate the given expression. Complex expressions is reduced to a literal expression
+     * while all other expressions are returned back immediately.
+     */
+    private fun calculate(expression: Expression, indexOfNextToCalculate: (Expression.Complex) -> Int): Expression {
+        if (expression !is Expression.Complex) {
+            return expression
         }
+        // calculate all parenthesis first
+        expression.list.forEachIndexed { index, expr -> expression.list[index] = calculate(expr, indexOfNextToCalculate) }
 
-        val remaining = expression
-        while (remaining is Expression.Complex && remaining.list.size > 1) {
-            // calculate all parenthesis first
-            remaining.list.mapIndexed { index, expr -> remaining.list[index] = calculate2(expr)}
+        // Reduce the complex expression one operator at a time until there is only one literal remaining.
+        while (expression.list.size > 1) {
+            calculateAtIndex(indexOfNextToCalculate(expression), expression)
+        }
+        return expression.list.first()
+    }
 
-            val plus = remaining.list.withIndex().firstOrNull { it.value == Expression.Operator('+') }
-            if (plus != null) {
-                val sum = (remaining.list[plus.index - 1] as Expression.Literal).value + (remaining.list[plus.index + 1] as Expression.Literal).value
-                remaining.list.removeAt(plus.index)
-                remaining.list.removeAt(plus.index)
-                remaining.list[plus.index - 1] = Expression.Literal(sum)
-                continue
-            } else {
-                // there is no plus remaining
-                val prod = (remaining.list[0] as Expression.Literal).value * (remaining.list[2]  as Expression.Literal).value
-                remaining.list.removeAt(1)
-                remaining.list.removeAt(1)
-                remaining.list[0] = Expression.Literal(prod)
-                continue
-            }
-        }
-        if (remaining is Expression.Complex) {
-            return remaining.list.first() // when a complex has been reduced to just one literal
-        }
-        return remaining
+    /**
+     * Calculates the result of the two literals and the in between operator at the given position in the given
+     * expression. The literals and operator is removed and replaced in the given expression.
+     */
+    private fun calculateAtIndex(index: Int, expression: Expression.Complex) {
+        val operator = expression.list[index + 1] as Expression.Operator
+        val res = operator.calculate(expression.list[index], expression.list[index + 2])
+        expression.list.removeAt(index)
+        expression.list.removeAt(index)
+        expression.list[index] = Expression.Literal(res)
+    }
+
+    /**
+     * As long as there are any pluses left calculate those first, otherwise calculate the left most pair
+     */
+    private fun plusFirstStrategy(expression: Expression.Complex): Int {
+        return max(expression.list.indexOf(Expression.Operator('+')) - 1, 0)
+    }
+
+    private fun solve(strategy: (Expression.Complex) -> Int): Long {
+        return problems.map { calculate(it, strategy) }.sumOf { (it as Expression.Literal).value }
+    }
+
+    fun solvePart1(): Long {
+        return solve { 0 } // always choose the leftmost pair to calculate first
     }
 
     fun solvePart2(): Long {
-        val x  = problems.map { parseInput(it) }.map { calculate2(it) }.sumOf { (it as Expression.Literal).value }
-        return x
+        return solve(this::plusFirstStrategy)
     }
 }
