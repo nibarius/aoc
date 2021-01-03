a336a4deebf344735a082731427623cbb5ed5824
Niklas Barsk <nibarius@gmail.com>
2021-01-03 21:53:14 +0100

    Rework 2018 day 18 to be more efficient
    * Uses numNeighboursWithValue which caches neighbours rather
      than generating new neighbours in each iteration
    * Use a Grid instead of AMap since the area is static.
    * Refactor the solution to be more generic instead of relying
      on the loop being present between iteration 500 and 600.
    * Reuse the same method for both part one and two instead of
      using two different that does almost the same thing.
    
    The first two points reduces runtime from slightly over a second to
    below 250ms.

--- src/main/aoc2018/Day18.kt
+++ src/main/aoc2018/Day18.kt
@@ -1,81 +1,61 @@
 package aoc2018
 
+import Grid
 import Pos
-import AMap
 
 class Day18(input: List<String>) {
-    private var area = AMap()
+    private var area = Grid.parse(input)
 
-    init {
-        for (y in input.indices) {
-            for (x in input[0].indices) {
-                area[Pos(x, y)] = input[y][x]
-            }
-        }
-    }
-
-    // Get all adjacent acres (x for acres outside the area)
-    private fun getAdjacent(pos: Pos): List<Char> {
-        return (-1..1).map { dy -> (-1..1).map { dx -> Pos(pos.x + dx, pos.y + dy) } }
-                .flatten()
-                .filterNot { it == pos }
-                .map { area.getOrDefault(it, 'x') }
-
-    }
-
-    private fun nextState(pos: Pos): Char {
-        val adjacent = getAdjacent(pos)
+    private fun nextState(area: Grid, pos: Pos): Char {
+        val trees = area.numNeighboursWithValue(pos, '|', true)
+        val yards = area.numNeighboursWithValue(pos, '#', true)
         return when (area[pos]) {
-            '.' -> if (adjacent.count { it == '|' } >= 3) '|' else '.'
-            '|' -> if (adjacent.count { it == '#' } >= 3) '#' else '|'
-            '#' -> if (adjacent.count { it == '#' } >= 1 && adjacent.count { it == '|' } >= 1) '#' else '.'
+            '.' -> if (trees >= 3) '|' else '.'
+            '|' -> if (yards >= 3) '#' else '|'
+            '#' -> if (yards >= 1 && trees >= 1) '#' else '.'
             else -> throw RuntimeException("Unknown character: ${area[pos]}")
         }
     }
 
-    private fun letTimePass(minutes: Int) {
-        repeat(minutes) {
-            val nextState = AMap()
-            area.keys.forEach { pos -> nextState[pos] = nextState(pos) }
-            area = nextState
+
+    /**
+     * Tries to find a loop within the given amount of interations. If
+     * a loop is found the first entry in the pair is the loop length and
+     * the second the scores or the loop. If no loop is found the second
+     * entry in the pair is a list containing only the score of the last iteration
+     */
+    private fun findLoop(maxIterations: Int): Pair<Int, List<Int>> {
+        val scores = mutableListOf<Int>() // index 0 is score for minute 1
+        val seen = mutableMapOf<Int, Int>() // hashcode to minute seen last
+        var current = area
+        var next = area.copy()
+        var tmp: Grid
+        for (minute in 1..maxIterations) {
+            current.keys.forEach { pos -> next[pos] = nextState(current, pos) }
+            tmp = current
+            current = next
+            next = tmp
+
+            val hash = current.hashCode()
+            if (seen.containsKey(hash)) { // loop found
+                return seen[hash]!! to scores.drop(seen[hash]!! - 1)
+            }
+            seen[hash] = minute
+            scores.add(getScore(current))
         }
+        return -1 to scores.takeLast(1)
     }
 
-    private fun findLoop(startAt: Int): List<Int> {
-        val scores = mutableListOf<Int>()
-        var numSame = 0
-        for (second in 1 until startAt + 100) {
-            val nextState = AMap()
-            area.keys.forEach { pos -> nextState[pos] = nextState(pos) }
-            area = nextState
-            val score = getScore()
-            val minLoopLength = 10
-
-            if (scores.isNotEmpty() && score == scores[0]) {
-                return scores
-            }
-            if (second >= startAt) {
-                scores.add(score)
-            }
-            if (second > startAt + minLoopLength && scores[numSame] == score) {
-                numSame++
-            }
-        }
-        throw java.lang.RuntimeException("No loop found")
-    }
-
-    private fun getScore() = area.values.count { it == '#' } * area.values.count { it == '|' }
+    private fun getScore(area: Grid) = area.count('#') * area.count('|')
 
     fun solvePart1(): Int {
-        letTimePass(10)
-        return getScore()
+        return findLoop(10).second.single()
     }
 
     fun solvePart2(): Int {
-        val startAt = 500
         val target = 1000000000
-        val loop = findLoop(startAt)
-        val index = (target - startAt) % loop.size
+        val (start, loop) = findLoop(target)
+        val index = (target - start) % loop.size
         return loop[index]
     }
 }
