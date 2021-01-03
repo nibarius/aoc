f7fab89931fb292893a40bac78fbf64a6485aa25
Niklas Barsk <nibarius@gmail.com>
2021-01-03 18:51:52 +0100

    Use a Grid instead of AMap for 2015 day 18
    It's using a fixed size so using two grids instead of many AMap copies
    increases speed with around 40%

--- src/main/aoc2015/Day18.kt
+++ src/main/aoc2015/Day18.kt
@@ -1,44 +1,47 @@
 package aoc2015
 
-import AMap
+import Grid
 import Pos
 
 class Day18(input: List<String>) {
 
-    private val initialState = AMap.parse(input)
+    private val initialState = Grid.parse(input)
     private val corners = initialState.let {
-        val x = it.xRange()
-        val y = it.yRange()
-        listOf(Pos(x.first, y.first),
-                Pos(x.first, y.last),
-                Pos(x.last, y.first),
-                Pos(x.last, y.last))
+        val last = it.size - 1
+        listOf(Pos(0, 0),
+                Pos(0, last),
+                Pos(last, 0),
+                Pos(last, last))
     }
 
-    private fun animate(steps: Int, frozenCorners: Boolean = false): AMap {
-        var current = initialState
+    private fun animate(steps: Int, frozenCorners: Boolean = false): Grid {
+        val a = initialState
+        val b = initialState.copy()
+        var current = a
+        var next = b
         repeat(steps) {
-            val next = current.copy()
             current.keys.forEach { pos ->
                 val neighbours = current.numNeighboursWithValue(pos, '#', includeDiagonals = true)
                 val on = current[pos] == '#'
-                when {
-                    frozenCorners && corners.contains(pos) -> next[pos] = '#'
-                    !on && neighbours == 3 -> next[pos] = '#'
-                    on && neighbours !in 2..3 -> next[pos] = '.'
+                next[pos] = when {
+                    frozenCorners && pos in corners -> '#'
+                    !on && neighbours == 3 -> '#'
+                    on && neighbours !in 2..3 -> '.'
+                    else -> current[pos]
                 }
             }
             current = next
+            next = if (current == a) b else a
         }
         return current
     }
 
     fun solvePart1(steps: Int = 100): Int {
-        return animate(steps).values.count { it == '#' }
+        return animate(steps).count('#')
     }
 
     fun solvePart2(steps: Int = 100): Int {
         corners.forEach { initialState[it] = '#' }
-        return animate(steps, frozenCorners = true).values.count { it == '#' }
+        return animate(steps, frozenCorners = true).count('#')
     }
 }
