7e00e90e89b4cefa80367f12824cc67e993b57b5
Niklas Barsk <nibarius@gmail.com>
2021-12-07 21:45:28 +0100

    Find the easter egg in 2021 day 7
    The input can actually be run as an intcode program which
    prints a message saying that it's not an intcode program.
    
    I really love this!

--- src/test/aoc2021/Day7Test.kt
+++ src/test/aoc2021/Day7Test.kt
@@ -1,9 +1,11 @@
 package test.aoc2021
 
+import aoc2019.Intcode
 import aoc2021.Day7
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import resourceAsList
+import resourceAsString
 
 class Day7Test {
     private val exampleInput = """16,1,2,0,4,2,7,1,2,14""".trimIndent().split("\n")
@@ -30,5 +32,14 @@ class Day7Test {
     fun partTwoRealInput() {
         val day7 = Day7(resourceAsList("2021/day7.txt"))
         assertEquals(104822130, day7.solvePart2())
+    }
+
+    @Test
+    fun easterEgg() {
+        // Reference to https://en.wikipedia.org/wiki/The_Treachery_of_Images
+        val intcode = Intcode(resourceAsString("2021/day7.txt").split(",").map { it.toLong() })
+        intcode.run()
+        val asciiOutput = intcode.output.joinToString(""){ it.toInt().toChar().toString() }
+        assertEquals("Ceci n'est pas une intcode program\n", asciiOutput )
     }
 }
