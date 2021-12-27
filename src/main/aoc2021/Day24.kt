4eeee3e27109ebfe7a7ce4f770326fc29987d2e6
Niklas Barsk <nibarius@gmail.com>
2021-12-27 20:49:55 +0100

    Solve 2021 day 24 programmatically
    Not to hard to solve programmatically when you've figured out what
    the program does. Using a stack instead of multiplying and dividing
    z with 26 also makes things easier.

--- src/main/aoc2021/Day24.kt
+++ src/main/aoc2021/Day24.kt
@@ -1,111 +1,84 @@
 package aoc2021
 
-import kotlin.system.measureNanoTime
-
 class Day24(input: List<String>) {
 
+    // The input has 14 chunks, one for each digit, of 18 instructions each where
+    // the 6th and 16th instructions varies and the rest are the same.
+    private val variantOne = input.chunked(18).map { it[5].split(" ").last().toInt() }
+    private val variantTwo = input.chunked(18).map { it[15].split(" ").last().toInt() }
 
-    /*
-chunk 0, inst 5: div z 1, inst 6: add x 15, inst 16: add y 9
-chunk 1, inst 5: div z 1, inst 6: add x 11, inst 16: add y 1
-chunk 2, inst 5: div z 1, inst 6: add x 10, inst 16: add y 11
-chunk 3, inst 5: div z 1, inst 6: add x 12, inst 16: add y 3
-chunk 4, inst 5: div z 26, inst 6: add x -11, inst 16: add y 10
-chunk 5, inst 5: div z 1, inst 6: add x 11, inst 16: add y 5
-chunk 6, inst 5: div z 1, inst 6: add x 14, inst 16: add y 0
-chunk 7, inst 5: div z 26, inst 6: add x -6, inst 16: add y 7
-chunk 8, inst 5: div z 1, inst 6: add x 10, inst 16: add y 9
-chunk 9, inst 5: div z 26, inst 6: add x -6, inst 16: add y 15
-chunk 10, inst 5: div z 26, inst 6: add x -6, inst 16: add y 4
-chunk 11, inst 5: div z 26, inst 6: add x -16, inst 16: add y 10
-chunk 12, inst 5: div z 26, inst 6: add x -4, inst 16: add y 4
-chunk 13, inst 5: div z 26, inst 6: add x -2, inst 16: add y 9
+    /**
+     * The result of running the program
+     * @param first The first digit in the input that might have to be modified
+     * @param second The second digit in the input that might have to be modified
+     * @param amount The amount the first digit needs to be increased, or alternatively how much the second digit
+     * must be decreased. If increase is zero the program completed as expected.
      */
-    /*init {
-        input.chunked(18).forEachIndexed { index, list ->
-            println("chunk $index, inst 5: ${list[4]}, inst 6: ${list[5]}, inst 16: ${list[15]}")
-        }
-    }*/
+    data class Result(val first: Int, val second: Int, val amount: Int)
 
-
-    fun theProgram2(input: List<Int>) {
-        var (w, x, y, z) = listOf(0L, 0, 0, 0)
-        val variantOne = listOf(15, 11, 10, 12, -11, 11, 14, -6, 10, -6, -6, -16, - 4, -2)
-        val variantTwo = listOf(9, 1, 11, 3, 10, 5, 0, 7, 9, 15, 4, 10, 4, 9)
-        println("with ${input.joinToString("")}")
+    /**
+     * A rewrite in Kotlin of the program given as puzzle input. For z to be 0 at the end x and w must
+     * be the same every time variantOne is negative. So this function returns early if that is not
+     * the case with instructions on how to modify the input in order for x == w to be true.
+     *
+     * The program pushes 7 values to a stack and pops 7 values. When it pops a value it compares
+     * if the current digit matches the corresponding one pushed earlier with some additions based
+     * on variantOne and variantTwo. Each character in the input is therefore connected to another
+     * character in the input and the diff between the characters is important. So the characters
+     * can have several accepted values as long as the diff between them are constant.
+     */
+    private fun theProgram4(input: List<Int>): Result {
+        var w: Int
+        var x: Int
+        val stack = mutableListOf(-1 to 0) // <digit that the current value is based on> to <value>
         repeat(14) { digit ->
-            w = input[digit].toLong()
-            x = z % 26
-            if (digit in listOf(4, 7, 9, 10, 11, 12, 13)) { // whenever variantOne is negative
-                z /= 26
+            w = input[digit]
+            x = stack.last().second % 26
+            val beforeRemoval = stack.last().first
+            if (variantOne[digit] < 0) {
+                stack.removeLast()
             }
             x += variantOne[digit]
-            println("x: $x, w: $w")
-            if (x != w) {
-                z *= 26
-                z += w + variantTwo[digit]
+            if (variantOne[digit] < 0 && x != w) {
+                // For z to be 0 at the end x must equal w every time variantOne is negative,
+                // if it isn't return how much the digit at 'beforeRemoval' or at 'digit'
+                // needs to be adjusted for x to equal w
+                return Result(beforeRemoval, digit, (w - x))
             }
-            println("After $digit ($w): z=$z")
-        }
 
-        println("Theprogram 2: $w, $x, $y, $z")
-        // desired: z == 0
-    }
-
-    fun theProgram3(input: List<Int>): Int {
-        var (w, x, y, z) = listOf(0L, 0, 0, 0)
-        val variantOne = listOf(15, 11, 10, 12, -11, 11, 14, -6, 10, -6, -6, -16, - 4, -2)
-        val variantTwo = listOf(9, 1, 11, 3, 10, 5, 0, 7, 9, 15, 4, 10, 4, 9)
-        var thisIsIt = false
-        repeat(14) { digit ->
-            w = input[digit].toLong()
-            x = z % 26
-            if (digit in listOf(4, 7, 9, 10, 11, 12, 13)) { // whenever variantOne is negative
-                z /= 26
-                thisIsIt = true
-            }
-            x += variantOne[digit]
-            if (thisIsIt && x != w) {
-                println("Break down at $digit, x: $x, w: $w")
-                // failed on 'digit'
-                return digit
-            }
-            thisIsIt = false
             if (x != w) {
-                z *= 26
-                z += w + variantTwo[digit]
+                stack.add(digit to w + variantTwo[digit])
             }
         }
-
-        println("Theprogram 2: $w, $x, $y, $z")
-        // desired: z == 0
-        return -1
+        return Result(0, 0, 0)
     }
 
 
     fun solvePart1(): Long {
-        // index 3: 9, index 4: 1
-        // index 6: 7, index 7: 1
-        // index 8: 6, index 9: 9
-        // index 10: 1
-        //val testInput =  "99991993698469".map { it.digitToInt() } // fails on last
-        val testInput =  "29991993698469".map { it.digitToInt() }
-        theProgram3(testInput)
-        return 29991993698469
+        // Start from all 9:s and decrease the required digit once an error is detected
+        val testInput = "99999999999999".map { it.digitToInt() }.toMutableList()
+        do {
+            val (first, second, amount) = theProgram4(testInput)
+            if (amount < 0) {
+                testInput[first] += amount
+            } else if (amount > 0) {
+                testInput[second] -= amount
+            }
+        } while (amount != 0)
+        return testInput.joinToString("").toLong()
     }
 
     fun solvePart2(): Long {
-        //v testInput =  "11191293691111".map { it.digitToInt() } // breaks down at digit 11
-        //l testInput =  "11191271141111".map { it.digitToInt() } // breaks down at 11
-        //                01234567890123
-        val testInput =  "14691271141118".map { it.digitToInt() }
-
-        // 67 can be 71, 82, 93
-        // 10 depends on 5
-        // 11 depends on 2
-        // 12 depends on 1
-        // 13 depends on 0n
-        theProgram3(testInput)
-        return 14691271141118
+        // Start from all 1:s and increase the required digit once an error is detected
+        val testInput = "11111111111111".map { it.digitToInt() }.toMutableList()
+        do {
+            val (first, second, amount) = theProgram4(testInput)
+            if (amount < 0) {
+                testInput[second] -= amount
+            } else if (amount > 0) {
+                testInput[first] += amount
+            }
+        } while (amount != 0)
+        return testInput.joinToString("").toLong()
     }
 }
