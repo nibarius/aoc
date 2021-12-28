071ef542302848d39b292f5292035e6f85ae4ab9
Niklas Barsk <nibarius@gmail.com>
2021-12-28 11:21:31 +0100

    Use two Grids instead of several AMaps for 2021 day 20
    It was possible to do a Grid based solution that didn't end
    up being messy. Only allocating two grids in total instead of a new
    AMap every iteration speeds up the total run time for part 1 + 2
    from 2.1 seconds to 900ms.

--- src/main/aoc2021/Day20.kt
+++ src/main/aoc2021/Day20.kt
@@ -1,12 +1,12 @@
 package aoc2021
 
-import AMap
+import Grid
 import Pos
 
 class Day20(input: List<String>) {
 
     private val algorithm = input.first()
-    private val initialImage = AMap.parse(input.drop(2))
+    private val initialGrid = Grid.parse(input.drop(2))
 
     private val allDeltas = buildList {
         for (y in -1..1) {
@@ -16,7 +16,7 @@ class Day20(input: List<String>) {
         }
     }
 
-    private fun lookupPixelFor(pos: Pos, image: AMap, default: Char): Char {
+    private fun lookupPixelFor(pos: Pos, image: Grid, default: Char): Char {
         val offset = allDeltas.map { image.getOrDefault(pos + it, default) }
             .joinToString("") { if (it == '#') "1" else "0" }
             .toInt(2)
@@ -24,21 +24,20 @@ class Day20(input: List<String>) {
         return algorithm[offset]
     }
 
-    private fun enhance(image: AMap, default: Char): AMap {
-        val yr = image.yRange().first - 1..image.yRange().last + 1
-        val xr = image.xRange().first - 1..image.xRange().last + 1
-        val enhancedImage = AMap()
-
-        for (y in yr) {
-            for (x in xr) {
-                enhancedImage[Pos(x, y)] = lookupPixelFor(Pos(x, y), image, default)
+    private fun enhance(read: Grid, write: Grid, default: Char, offset: Int) {
+        for (y in offset until read.size - offset) {
+            for (x in offset until read.size - offset) {
+                write[Pos(x, y)] = lookupPixelFor(Pos(x, y), read, default)
             }
         }
-        return enhancedImage
     }
 
+    // Each enhance grows the grid with one in each direction (+2 horizontally and +2 vertically each turn).
+    // Create grid that has the size of the gird after all enhancements are done and put the initial grid
+    // in the middle of that grid, then just increase the area that's being read from /written to each iteration.
     private fun repeatedEnhance(times: Int): Int {
-        var next = initialImage
+        val a = Grid(initialGrid.size + 2 * times).apply { writeAllWithOffset(initialGrid, Pos(times, times)) }
+        val grids = listOf(a, a.copy())
         repeat(times) {
             // Example input algorithm starts with . which means 9 blank pixels results in a blank pixel
             // Real input starts with # and ends with . which means that 9 blank pixels becomes a lit pixel
@@ -46,12 +45,11 @@ class Day20(input: List<String>) {
             // iteration. This will not work with an algorithm that start and ends with '#', but there is no
             // such input so that doesn't matter.
             val default = if (algorithm.first() == '#' && it % 2 == 1) '#' else '.'
-            next = enhance(next, default)
+            enhance(grids[it % 2], grids[(it + 1) % 2], default, times - it - 1)
         }
-        return next.values.count { it == '#' }
+        return grids[times % 2].count('#')
     }
 
-    // reddit test starts with #, example input starts with .
     fun solvePart1(): Int {
         return repeatedEnhance(2)
     }
