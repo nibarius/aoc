b343f38c8505ed4acc9cb59968b7c0b8efdfdbf2
Niklas Barsk <nibarius@gmail.com>
2022-12-01 08:02:41 +0100

    2022 day 1: Calorie Counting
    Felt a bit rusty and didn't realize that I could work on the
    input in another format than the one where each line is one
    entry in the list. Things were much simpler when splitting
    on double newlines instead of single newlines.

--- src/main/aoc2022/Day1.kt
+++ src/main/aoc2022/Day1.kt
@@ -1,12 +1,23 @@
 package aoc2022
 
+/**
+ * @param input A list of elves, each entry is a newline separated string with calorie data
+ * for each item the elf is carrying.
+ */
 class Day1(input: List<String>) {
 
+    /**
+     * A list of total calories per elf.
+     */
+    private val parsedInput = input.map { elf ->
+        elf.split("\n").sumOf { calories -> calories.toInt() }
+    }
+
     fun solvePart1(): Int {
-        return -1
+        return parsedInput.maxOf { it }
     }
 
     fun solvePart2(): Int {
-        return -1
+        return parsedInput.sortedDescending().take(3).sum()
     }
 }
--- src/test/aoc2022/Day1Test.kt
+++ src/test/aoc2022/Day1Test.kt
@@ -4,31 +4,47 @@ import aoc2022.Day1
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import resourceAsList
+import resourceSplitOnBlankLines
 
 class Day1Test {
-    private val exampleInput = """""".trimIndent().split("\n")
+    private val exampleInput = """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """.trimIndent().split("\n\n")
 
     @Test
     fun testPartOneExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart1())
+        assertEquals(24000, day1.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
-        val day1 = Day1(resourceAsList("2022/day1.txt"))
-        assertEquals(0, day1.solvePart1())
+        val day1 = Day1(resourceSplitOnBlankLines("2022/day1.txt"))
+        assertEquals(69836, day1.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart2())
+        assertEquals(45000, day1.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
-        val day1 = Day1(resourceAsList("2022/day1.txt"))
-        assertEquals(0, day1.solvePart2())
+        val day1 = Day1(resourceSplitOnBlankLines("2022/day1.txt"))
+        assertEquals(207968, day1.solvePart2())
     }
 }
