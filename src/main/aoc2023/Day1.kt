96759dcbe95b2edbd5346df5c53a937c4bdd75a6
Niklas Barsk <nibarius@gmail.com>
2023-12-01 21:59:19 +0100

    2023 Day 1: Trebuchet?!
    First part was easy as expected. Second part took around an hour.
    Probably most difficult first day yet.

--- src/main/aoc2023/Day1.kt
+++ src/main/aoc2023/Day1.kt
@@ -1,12 +1,43 @@
 package aoc2023
 
-class Day1(input: List<String>) {
+class Day1(val input: List<String>) {
+
+    /**
+     * Search character by character for the first digit in the given order
+     */
+    private fun findFirstDigit(line: String, iterationOrder: IntProgression): Char {
+        val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+        for (lineOffset in iterationOrder) {
+            if (line[lineOffset] in '1'..'9') {
+                return line[lineOffset]
+            }
+            numbers.forEachIndexed { index, textNumber ->
+                if (line.drop(lineOffset).startsWith(textNumber)) {
+                    return '1' + index
+                }
+            }
+        }
+        error("Should not happen")
+    }
+
+    private fun getCalibrationValueWithText(line: String): Int {
+        val first = findFirstDigit(line, line.indices)
+        val last = findFirstDigit(line, line.indices.reversed())
+        return "$first$last".toInt()
+    }
+
+    private fun getCalibrationValue(line: String): Int {
+        val first = line.first { it in '1'..'9' }
+        val last = line.last { it in '1'..'9' }
+        return "$first$last".toInt()
+    }
 
     fun solvePart1(): Int {
-        return -1
+        return input.sumOf { getCalibrationValue(it) }
     }
 
     fun solvePart2(): Int {
-        return -1
+        return input.sumOf { getCalibrationValueWithText(it) }
     }
 }
--- src/test/aoc2023/Day1Test.kt
+++ src/test/aoc2023/Day1Test.kt
@@ -6,29 +6,54 @@ import org.junit.Test
 import resourceAsList
 
 class Day1Test {
-    private val exampleInput = """""".trimIndent().split("\n")
+    private val exampleInput = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent().split("\n")
+
+    private val exampleInput2 = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent().split("\n")
 
     @Test
     fun testPartOneExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart1())
+        assertEquals(142, day1.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
         val day1 = Day1(resourceAsList("2023/day1.txt"))
-        assertEquals(0, day1.solvePart1())
+        assertEquals(54561, day1.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
-        val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart2())
+        val day1 = Day1(exampleInput2)
+        assertEquals(281, day1.solvePart2())
+    }
+    @Test
+    fun testPartTwoExtraTest() {
+        // Index of the last three is before the two
+        // 1threethree2three
+        // 133two3
+        // So generating 9 strings, each with one digit replaced and then looking for the
+        // largest index won't work.
+        val day1 = Day1(listOf("1threethreetwothree"))
+        assertEquals(13, day1.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
         val day1 = Day1(resourceAsList("2023/day1.txt"))
-        assertEquals(0, day1.solvePart2())
+        assertEquals(54076, day1.solvePart2())
     }
 }
