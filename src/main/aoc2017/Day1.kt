9018fc2048353974434b10a6d5596eb81feab182
Niklas Barsk <nibarius@gmail.com>
2024-11-29 22:12:44 +0100

    2017 Day 1: Inverse Captcha
    A bit of practice before 2024 starts, quick and easy.

--- src/main/aoc2017/Day1.kt
+++ src/main/aoc2017/Day1.kt
@@ -1,12 +1,16 @@
 package aoc2017
 
-class Day1(input: List<String>) {
+class Day1(val input: String) {
+
+    private fun solveCaptcha(distance: Int) = input.mapIndexed { index, c ->
+        if (c == input[(index + distance) % input.length]) c.digitToInt() else 0
+    }.sum()
 
     fun solvePart1(): Int {
-        return -1
+        return solveCaptcha(1)
     }
 
     fun solvePart2(): Int {
-        return -1
+        return solveCaptcha(input.length / 2)
     }
 }
--- src/test/aoc2017/Day1Test.kt
+++ src/test/aoc2017/Day1Test.kt
@@ -1,27 +1,63 @@
 package test.aoc2017
 
+import aoc2017.Day1
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import resourceAsString
 
 class Day1Test {
     @Test
     fun testPartOneExample1() {
-        val input = """""".trimIndent().split("\n")
-        //assertEquals(0, Day1(input).solvePart1())
+        assertEquals(3, Day1("1122").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample2() {
+        assertEquals(4, Day1("1111").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample3() {
+        assertEquals(0, Day1("1234").solvePart1())
+    }
+
+    @Test
+    fun testPartOneExample4() {
+        assertEquals(9, Day1("91212129").solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
-        //assertEquals(0, Day1(resourceAsList("2017/day1.txt")).solvePart1())
+        assertEquals(1228, Day1(resourceAsString("2017/day1.txt")).solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
-        val input = """""".trimIndent().split("\n")
-        //assertEquals(0, Day1(input).solvePart2())
+        assertEquals(6, Day1("1212").solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample2() {
+        assertEquals(0, Day1("1221").solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample3() {
+        assertEquals(4, Day1("123425").solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample4() {
+        assertEquals(12, Day1("123123").solvePart2())
+    }
+
+    @Test
+    fun testPartTwoExample5() {
+        assertEquals(4, Day1("12131415").solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
-        //assertEquals(0, Day1(resourceAsList("2017/day1.txt")).solvePart2())
+        assertEquals(1238, Day1(resourceAsString("2017/day1.txt")).solvePart2())
     }
 }
