44c73e60140ae715eb6dc82f938fe90d3c333aa9
Niklas Barsk <nibarius@gmail.com>
2024-12-01 20:09:34 +0100

    2024 Day 1: Historian Hysteria
    Pretty simple day 1 this year compared to last year. Learned about
    unzip() this time, not surprising that there is one since there is
    a zip() method.

--- src/main/aoc2024/Day1.kt
+++ src/main/aoc2024/Day1.kt
@@ -1,12 +1,27 @@
 package aoc2024
 
+import kotlin.math.abs
+
 class Day1(input: List<String>) {
+    val left: List<Int>
+    val right: List<Int>
+
+    init {
+        val (l, r) = input.map { line ->
+            line.split("   ")
+                .let { it.first().toInt() to it.last().toInt() }
+        }.unzip()
+        left = l
+        right = r
+    }
 
     fun solvePart1(): Int {
-        return -1
+        return left.sorted().zip(right.sorted()).sumOf {
+            abs(it.second - it.first)
+        }
     }
 
     fun solvePart2(): Int {
-        return -1
+        return left.sumOf { l -> l * right.count { it == l } }
     }
 }
--- src/test/aoc2024/Day1Test.kt
+++ src/test/aoc2024/Day1Test.kt
@@ -6,29 +6,36 @@ import org.junit.Test
 import resourceAsList
 
 class Day1Test {
-    private val exampleInput = """""".trimIndent().split("\n")
+    private val exampleInput = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent().split("\n")
 
     @Test
     fun testPartOneExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart1())
+        assertEquals(11, day1.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
         val day1 = Day1(resourceAsList("2024/day1.txt"))
-        assertEquals(0, day1.solvePart1())
+        assertEquals(1258579, day1.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart2())
+        assertEquals(31, day1.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
         val day1 = Day1(resourceAsList("2024/day1.txt"))
-        assertEquals(0, day1.solvePart2())
+        assertEquals(23981443, day1.solvePart2())
     }
 }
