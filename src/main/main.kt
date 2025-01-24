e167cd5f0a92d0c68f69d47830d54f04d71bf335
Niklas Barsk <nibarius@gmail.com>
2025-01-24 20:30:12 +0100

    2024 Day 25: Code Chronicle
    Pretty easy last day. Most time was spent writing the parsing
    code, with that in place the rest was very easy.
    
    Fun year! Some challenging days, but nothing where I felt completely
    lost. Got some more experience with dynamic programming this year,
    hope I remember it next year.

--- src/main/main.kt
+++ src/main/main.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val year = 2024
-    val day = 24
+    val day = 25
     when (args.firstOrNull()) {
         "download" -> readInputFileFromInternet(year, day)
         "decrypt" -> decryptInputFile(year, day)
