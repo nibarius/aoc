f4a795d8366d1eba871cbbc5e09ba9bb27f56a4e
Niklas Barsk <nibarius@gmail.com>
2025-12-01 21:53:35 +0100

    2025 Day 1: Secret Entrance
    First part was pretty easy, but the second was hard to get right.
    My initial solution was to just brute force it by turning one step
    at a time. But then I rewrote it into the current solution where
    it first turns to 0/99 in one go, does a one position turn to wrap
    around and then turn the remaining distance (or to 0/99 again).
    
    This made it much easier to understand what's happening and to make
    sure the code is actually working.

--- src/main/aoc2025/Day1.kt
+++ src/main/aoc2025/Day1.kt
@@ -1,12 +1,63 @@
 package aoc2025
 
+import kotlin.math.max
+import kotlin.math.min
+
 class Day1(input: List<String>) {
+    private val start = 50
+    private val turns = input.map {
+        if (it.startsWith("L")) {
+            -it.drop(1).toInt()
+        } else
+            it.drop(1).toInt()
+    }
+
+    /**
+     * Recursively turn the dial the given amount of steps by first turning to
+     * 0 or 99, then turning one step to wrap around and finally continue with
+     * the remaining steps.
+     */
+    private fun turn22(from: Int, amount: Int, zeroVisits: Int): Pair<Int, Int> {
+        if (amount == 0) {
+            return from to zeroVisits
+        } else if (from == 0 && amount < 0) {
+            return turn22(99, amount + 1, zeroVisits)
+        } else if (from == 99 && amount > 0) {
+            return turn22(0, amount - 1, zeroVisits + 1)
+        }
+
+        val turningThisTime = if (amount < 0) {
+            max(amount, 0 - from) // turn at most down to zero
+        } else {
+            min(amount, 99 - from) // turn at most up to 99
+        }
+        val nextFrom = from + turningThisTime
+        return turn22(
+            nextFrom,
+            amount - turningThisTime,
+            zeroVisits + if (nextFrom == 0) 1 else 0
+        )
+    }
 
     fun solvePart1(): Int {
-        return -1
+        var current = start
+        return turns.count { turn ->
+            turn22(current, turn, 0).let { (nextPos, _) ->
+                current = nextPos
+            }
+            current == 0
+        }
     }
 
     fun solvePart2(): Int {
-        return -1
+        var current = start
+        var zeroVisits = 0
+        turns.forEach { turn ->
+            turn22(current, turn, zeroVisits).let { (nextPos, zeroVisitsSoFar) ->
+                current = nextPos
+                zeroVisits = zeroVisitsSoFar
+            }
+        }
+        return zeroVisits
     }
 }
--- src/test/aoc2025/Day1Test.kt
+++ src/test/aoc2025/Day1Test.kt
@@ -6,29 +6,40 @@ import org.junit.Test
 import resourceAsList
 
 class Day1Test {
-    private val exampleInput = """""".trimIndent().split("\n")
+    private val exampleInput = """
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    """.trimIndent().split("\n")
 
     @Test
     fun testPartOneExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart1())
+        assertEquals(3, day1.solvePart1())
     }
 
     @Test
     fun partOneRealInput() {
         val day1 = Day1(resourceAsList("2025/day1.txt"))
-        assertEquals(0, day1.solvePart1())
+        assertEquals(1007, day1.solvePart1())
     }
 
     @Test
     fun testPartTwoExample1() {
         val day1 = Day1(exampleInput)
-        assertEquals(0, day1.solvePart2())
+        assertEquals(6, day1.solvePart2())
     }
 
     @Test
     fun partTwoRealInput() {
         val day1 = Day1(resourceAsList("2025/day1.txt"))
-        assertEquals(0, day1.solvePart2())
+        assertEquals(5820, day1.solvePart2())
     }
 }
